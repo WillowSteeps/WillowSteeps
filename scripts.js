7d635f5a64b7f73ad4887082625cb75c6ceb9871
WIllowSteeps <43142299+WillowSteeps@users.noreply.github.com>
2024-05-30 16:55:26 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,25 +1,21 @@
-// Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// Import Firebase scripts and Firestore
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js';
+import { getFirestore, collection, addDoc, getDocs } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js';
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
+// Your Firebase configuration
+const firebaseConfig = {
     apiKey: "AIzaSyAMOc6n_-UxqouQ8YcPb21cZ7tA79ISRSM",
     authDomain: "willowsteeps-fcca5.firebaseapp.com",
     projectId: "willowsteeps-fcca5",
     storageBucket: "willowsteeps-fcca5.appspot.com",
     messagingSenderId: "814830989524",
     appId: "1:814830989524:web:5b55c19b4155e58564cb41",
-    measurementId: "G-ZLP5BZGBLQ"
-  };
+    measurementId: "G-ZLP5BZGBLQ""
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</scrip
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 document.addEventListener('DOMContentLoaded', () => {
     const reviewForm = document.getElementById('review-form');
